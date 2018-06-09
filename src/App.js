f3bb24f3e27c8d8fbf2c3a5c39a52d0013d9a922
Ryuunofire27 <cdvillagomez27@gmail.com>
2018-06-09 14:23:50 -0500

    React-01

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import SignIn from './Components/SignIn'
 
 class App extends Component{
+  state = {
+    name: "Joseph",
+    email: "",
+    password: ""
+  }
+
+  changingName = () => {
+    this.setState({
+      name: 'OpenLab'
+    })
+  }
+  
+  setNameAndPassword = (user) => {
+    this.setState({
+      email: user.email,
+      password: user.password
+    })
+  }
   render(){
     return (
-      <h1>Hola mundo</h1>
+      <div>
+        <h1>{this.state.name}</h1>
+        {this.state.email !== "" && <h2>{this.state.email}</h2>}
+        {this.state.password !== "" && <h2>{this.state.password}</h2>}
+        <SignIn 
+          name={this.state.name} 
+          changeName={this.changingName}
+          setNameAndPassword={this.setNameAndPassword}
+          />
+      </div>
     );
   }
 }
 
-const mountDiv = document.getElementById('app');
+const mountDiv = document.querySelector('#app');
 
 render(<App/>, mountDiv);
